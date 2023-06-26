6dc405eaa5cb0da34dcb373611f886323971203c
Clara Uktar <clara.uktar@student.hpi.uni-potsdam.de>
2023-06-26 22:01:25 +0200

    Add basic RNA import

--- pepti_map/importing/peptide_importer.py
+++ pepti_map/importing/peptide_importer.py
@@ -5,5 +5,5 @@ def import_file(file_path: str) -> List[str]:
     peptides: List[str] = []
     with open(file_path) as peptide_file:
         for line in peptide_file:
-            peptides.append(line.rstrip())
+            peptides.append(line.strip())
     return peptides
--- pepti_map/importing/rna_importer.py
+++ pepti_map/importing/rna_importer.py
@@ -1,2 +1,33 @@
-def import_file(file_path: str):
-    pass
+import pandas as pd
+
+
+# TODO: Give option to import multiple files
+def import_file(file_path: str) -> pd.DataFrame:
+    with open(file_path) as rna_file:
+        df_data = []
+        line_count_for_current_sequence: int = 0
+        id = ""
+        sequence = ""
+        quality = ""
+
+        for line in rna_file:
+            if line_count_for_current_sequence == 0:
+                id = line.strip()
+            elif line_count_for_current_sequence == 1:
+                sequence = line.strip()
+            # Information from field 3 is not needed
+            elif line_count_for_current_sequence == 3:
+                quality = line.strip()
+
+            line_count_for_current_sequence = line_count_for_current_sequence + 1
+
+            # Always read 4 lines per sequence, as per FASTQ format
+            if line_count_for_current_sequence == 4:
+                df_data.append([id, sequence, quality])
+                line_count_for_current_sequence = 0
+                id = ""
+                sequence = ""
+                quality = ""
+
+        rna_df = pd.DataFrame(df_data, columns=["id", "sequence", "quality"])
+        return rna_df
