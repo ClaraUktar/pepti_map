6573be312944b65d9f808a5b4d289c0696ff142e
Clara Uktar <clara.uktar@student.hpi.uni-potsdam.de>
2023-08-15 14:05:27 +0100

    Add tests for peptide kmer index

--- pepti_map/importing/peptide_import/test_peptide_importer.py
+++ pepti_map/importing/peptide_import/test_peptide_importer.py
@@ -3,79 +3,40 @@ import pandas as pd
 import pytest
 
 from pepti_map.importing.peptide_import.peptide_importer import PeptideImporter
+from pepti_map.importing.peptide_import.peptide_to_index_importer import (
+    PeptideToIndexImporter,
+)
+from pepti_map.importing.peptide_import.testdata_peptide_importer import (
+    EXPECTED_RESULT_DF_BASIC,
+    EXPECTED_RESULT_DF_DEDUPLICATED,
+    EXPECTED_RESULT_INDEX,
+    MOCK_FILE_CONTENT,
+)
 
 
 class TestPeptideSimpleFormatImport:
-    # TODO: Extract into separate data file
-    # Test data was randomly generated
-    mock_file_content = """GQLDR
-    NCYQKAQHLYTPEGRKGMVHLTWDRTVLPPPCMDIVDRHRSSRY
-    QIPCI
-    LSVRCEVQCHWDYDLPVMRHKTFLAPCHTWM
-    YCYRSEDLLKISQQCARHRKAQPWETYVCIRTFTPHTMMHE
-    FNQGVRDQR
-    ISNVGQYSQMLSEFEFCKKVYCSCEFDVDRGSQICSDDHVWSPKKSPKMC
-    SHDTY
-    YCYRSEDLLKISQQCARHRKAQPWETYVCIRTFTPHTMMHE
-    FMRPCQFFHCWMFSMDDH"""
-
-    expected_result_df_deduplicated = pd.DataFrame(
-        {
-            "ids": [[0], [1], [2], [3], [4, 8], [5], [6], [7], [9]],
-            "sequence": [
-                "GQLDR",
-                "NCYQKAQHLYTPEGRKGMVHLTWDRTVLPPPCMDIVDRHRSSRY",
-                "QIPCI",
-                "LSVRCEVQCHWDYDLPVMRHKTFLAPCHTWM",
-                "YCYRSEDLLKISQQCARHRKAQPWETYVCIRTFTPHTMMHE",
-                "FNQGVRDQR",
-                "ISNVGQYSQMLSEFEFCKKVYCSCEFDVDRGSQICSDDHVWSPKKSPKMC",
-                "SHDTY",
-                "FMRPCQFFHCWMFSMDDH",
-            ],
-            "count": [1, 1, 1, 1, 2, 1, 1, 1, 1],
-        }
-    ).astype(dtype={"ids": "object", "sequence": "string", "count": "uint32"})
-
-    expected_result_basic = pd.DataFrame(
-        {
-            "sequence": [
-                "GQLDR",
-                "NCYQKAQHLYTPEGRKGMVHLTWDRTVLPPPCMDIVDRHRSSRY",
-                "QIPCI",
-                "LSVRCEVQCHWDYDLPVMRHKTFLAPCHTWM",
-                "YCYRSEDLLKISQQCARHRKAQPWETYVCIRTFTPHTMMHE",
-                "FNQGVRDQR",
-                "ISNVGQYSQMLSEFEFCKKVYCSCEFDVDRGSQICSDDHVWSPKKSPKMC",
-                "SHDTY",
-                "YCYRSEDLLKISQQCARHRKAQPWETYVCIRTFTPHTMMHE",
-                "FMRPCQFFHCWMFSMDDH",
-            ]
-        }
-    ).astype(dtype={"sequence": "string"})
-
     @pytest.fixture(autouse=True)
     def _init_peptide_importer(self):
         self.peptide_importer = PeptideImporter()
 
     def test_import_as_df_with_duplicates(self):
         with patch(
-            "builtins.open", mock_open(read_data=self.mock_file_content)
+            "builtins.open", mock_open(read_data=MOCK_FILE_CONTENT)
         ) as peptide_file_mock:
             result_df = self.peptide_importer.import_file_with_deduplication(
                 "path/to/file"
             )
-            pd.testing.assert_frame_equal(
-                result_df, self.expected_result_df_deduplicated
-            )
+            pd.testing.assert_frame_equal(result_df, EXPECTED_RESULT_DF_DEDUPLICATED)
         peptide_file_mock.assert_called_with("path/to/file", "rt", encoding="utf-8")
 
-    @patch("builtins.open", mock_open(read_data=mock_file_content))
+    @patch("builtins.open", mock_open(read_data=MOCK_FILE_CONTENT))
     def test_basic_import(self):
         result_df = self.peptide_importer.import_file("path/to/file")
-        pd.testing.assert_frame_equal(result_df, self.expected_result_basic)
+        pd.testing.assert_frame_equal(result_df, EXPECTED_RESULT_DF_BASIC)
 
 
 class TestPeptideToIndexImporter:
-    # TODO
-    pass
+    @patch("builtins.open", mock_open(read_data=MOCK_FILE_CONTENT))
+    def test_import_file_to_index(self):
+        resulting_index = PeptideToIndexImporter().import_file_to_index("path/to/file")
+        assert resulting_index.kmer_index == EXPECTED_RESULT_INDEX
--- pepti_map/peptide_data/test_peptide_kmer_index.py
+++ pepti_map/peptide_data/test_peptide_kmer_index.py
@@ -1,1 +1,21 @@
-# TODO
+from collections import defaultdict
+from pathlib import Path
+from pepti_map.importing.peptide_import.testdata_peptide_importer import (
+    EXPECTED_RESULT_INDEX,
+)
+from pepti_map.peptide_data.peptide_kmer_index import PeptideKmerIndex
+
+
+class TestPeptideKmerIndex:
+    def test_dump_and_load_index_file(self, tmp_path):
+        kmer_index = PeptideKmerIndex()
+        assert kmer_index.kmer_index == {}
+
+        kmer_index.kmer_index = defaultdict(list, EXPECTED_RESULT_INDEX.copy())
+        temp_directory: Path = tmp_path / "kmer_index"
+        temp_directory.mkdir()
+        file_path = temp_directory.as_posix() + "/index.txt"
+        kmer_index.dump_index_to_file(file_path)
+
+        new_kmer_index = PeptideKmerIndex.load_index_from_file(file_path)
+        assert new_kmer_index.kmer_index == EXPECTED_RESULT_INDEX
