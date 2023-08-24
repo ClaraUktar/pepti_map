6b1105cd30d9c6a7b702edf83adc1d0c038687d2
Clara Uktar <clara.uktar@student.hpi.uni-potsdam.de>
2023-08-24 18:22:10 +0100

    Only use set in matches if it is non-empty

--- pepti_map/matching/rna_to_peptide_matcher.py
+++ pepti_map/matching/rna_to_peptide_matcher.py
@@ -1,4 +1,4 @@
-from typing import Generator, List, Set
+from typing import Generator, List, Set, Union
 
 from pepti_map.peptide_data.peptide_kmer_index import PeptideKmerIndex
 from pepti_map.util.k_mer import split_into_kmer
@@ -8,7 +8,9 @@ from pepti_map.util.three_frame_translation import get_three_frame_translations
 class RNAToPeptideMatcher:
     def __init__(self, kmer_index: PeptideKmerIndex, number_of_peptides: int):
         self.kmer_index: PeptideKmerIndex = kmer_index
-        self.matches: List[Set[int]] = [set() for _ in range(0, number_of_peptides)]
+        self.matches: List[Union[Set[int], None]] = [
+            None for _ in range(0, number_of_peptides)
+        ]
 
     def _process_rna_read_to_kmers(self, sequence: str) -> Generator[str, None, None]:
         # TODO: Exchange all T for U? (inplace?)
@@ -23,4 +25,8 @@ class RNAToPeptideMatcher:
         for kmer in self._process_rna_read_to_kmers(rna_read_sequence):
             peptide_matches = self.kmer_index.getEntryForKmer(kmer)
             for match in peptide_matches:
-                self.matches[match].add(rna_read_id)
+                if self.matches[match] is None:
+                    self.matches[match] = set()
+                self.matches[match].add(  # pyright: ignore[reportOptionalMemberAccess]
+                    rna_read_id
+                )
