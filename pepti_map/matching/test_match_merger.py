8e2176a1f74e2824f8b6449f577630d03af43d50
Clara Uktar <clara.uktar@student.hpi.uni-potsdam.de>
2023-08-28 23:33:41 +0100

    Add test for edge case of late merge detection

--- pepti_map/matching/test_match_merger.py
+++ pepti_map/matching/test_match_merger.py
@@ -26,5 +26,32 @@ class TestMatchMerger:
             [[0, 2], [1], [3, 5], [4]],
         )
 
+    def test_matches_with_empty_sets(self):
+        merge_results = MatchMerger(
+            [
+                {1, 2, 3},
+                None,
+                {6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17},
+                {6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 20, 21},
+                None,
+            ]
+        ).merge_matches()
+        assert merge_results == (
+            [{1, 2, 3}, {6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 20, 21}],
+            [[0], [2, 3]],
+        )
+
+    def test_low_jaccard_index(self):
+        merge_results = MatchMerger(
+            [{1, 2, 3}, {3, 4, 5, 6}, {5, 6, 7, 8}, {8, 9, 10, 11}, {7, 8, 10, 12}],
+            jaccard_index_threshold=0.3,
+        ).merge_matches()
+        print("\n\nMERGE RESULTS:")
+        print(merge_results)
+        assert merge_results == (
+            [{1, 2, 3}, {3, 4, 5, 6, 7, 8, 9, 10, 11, 12}],
+            [[0], [1, 2, 3, 4]],
+        )
+
     # TODO: Add more tests
     # e.g. for matches list with None in it, different jaccard index
