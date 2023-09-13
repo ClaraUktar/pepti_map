b2433b5f239d9ea0f87229ea5237ff10a27c65f5
Clara Uktar <clara.uktar@student.hpi.uni-potsdam.de>
2023-09-13 13:14:16 +0100

    Add tests for new match merger

--- pepti_map/matching/test_match_merger.py
+++ pepti_map/matching/test_match_merger.py
@@ -1,7 +1,17 @@
-from unittest.mock import MagicMock
+from typing import Set
+from unittest.mock import patch
+from datasketch import LeanMinHash, MinHash
 import numpy as np
 
-from pepti_map.matching.match_merger import MatchMerger
+from pepti_map.matching.match_merger import NUM_BYTES_FOR_MIN_HASH_VALUES, MatchMerger
+
+
+def generate_min_hash_for_set(test_set: Set) -> LeanMinHash:
+    test_min_hash = MinHash()
+    test_min_hash.update_batch(
+        [elem.to_bytes(NUM_BYTES_FOR_MIN_HASH_VALUES, "big") for elem in test_set]
+    )
+    return LeanMinHash(test_min_hash)
 
 
 class TestMatchMerger:
@@ -11,7 +21,12 @@ class TestMatchMerger:
         match_merger = MatchMerger(test_matches)
         assert match_merger.matches == filtered_matches
 
-    def test_filter_out_none_entries_precomputed_intersections(self):
+    @patch(
+        "pepti_map.matching.match_merger.MatchMerger._create_exact_jaccard_calculator"
+    )
+    def test_filter_out_none_entries_precomputed_intersections(
+        self, mocked_create_exact_jaccard_calculator
+    ):
         test_matches = [
             {1, 2, 3, 4},
             {20, 30},
@@ -44,10 +59,75 @@ class TestMatchMerger:
                 [0, 0, 1, 4],
             ]
         )
-        MatchMerger._create_exact_jaccard_calculator = MagicMock()
         match_merger = MatchMerger(test_matches, 0.7, test_precomputed_intersections)
         assert match_merger.matches == filtered_matches
         np.testing.assert_array_equal(
-            MatchMerger._create_exact_jaccard_calculator.call_args.args[0],
+            mocked_create_exact_jaccard_calculator.call_args.args[0],
             filtered_intersections,
+        )
+
+    @patch(
+        (
+            "pepti_map.matching.jaccard_index_calculation.min_hash_calculator"
+            ".MinHashCalculator.__init__"
+        ),
+        return_value=None,
+    )
+    def test_min_hash_creation(self, mock_min_hash_calculator_init):
+        test_matches = [
+            {1, 2, 3, 4},
+            {20, 30},
+            None,
+            {3, 4, 7, 8},
+            {8, 10, 11, 12},
+            None,
+        ]
+        expected_min_hashes = [
+            generate_min_hash_for_set(match)
+            for match in test_matches
+            if match is not None
+        ]
+        MatchMerger(test_matches, 0.7)
+        mock_min_hash_calculator_init.assert_called_once()
+        assert mock_min_hash_calculator_init.call_args.args[0] == expected_min_hashes
+
+    @patch(
+        (
+            "pepti_map.matching.jaccard_index_calculation.exact_jaccard_calculator"
+            ".ExactJaccardCalculator.__init__"
+        ),
+        return_value=None,
+    )
+    def test_exact_jaccard_calculation(self, mock_exact_jaccard_calculator_init):
+        test_matches = [
+            {1, 2, 3, 4},
+            {20, 30},
+            None,
+            {3, 4, 7, 8},
+            {8, 10, 11, 12},
+            None,
+        ]
+        test_precomputed_intersections = np.array(
+            [
+                [4, 0, 0, 2, 0, 0],
+                [0, 2, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [2, 0, 0, 4, 1, 0],
+                [0, 0, 0, 1, 4, 0],
+                [0, 0, 0, 0, 0, 0],
+            ]
+        )
+        expected_jaccard_indexes = np.array(
+            [
+                [1.0, 0.0, 1 / 3, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [1 / 3, 0.0, 1.0, 1 / 7],
+                [0.0, 0.0, 1 / 7, 1.0],
+            ]
+        )
+        MatchMerger(test_matches, 0.7, test_precomputed_intersections)
+        mock_exact_jaccard_calculator_init.assert_called_once()
+        np.testing.assert_array_equal(
+            mock_exact_jaccard_calculator_init.call_args.args[0],
+            expected_jaccard_indexes,
         )
