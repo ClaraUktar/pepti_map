d14488441c8fba512f1e737fb225fe21bdbe8cc3
Clara Uktar <clara.uktar@student.hpi.uni-potsdam.de>
2023-10-16 13:43:47 +0200

    Use pyfastx to retrieve RNA-seq reads

--- pepti_map/importing/rna_import/test_rna_importer.py
+++ pepti_map/importing/rna_import/test_rna_importer.py
@@ -1,3 +1,4 @@
+import gzip
 from pathlib import Path
 from unittest.mock import mock_open, patch
 from io import StringIO
@@ -162,9 +163,13 @@ class TestRNAReadsRetriever:
         with pytest.raises(ValueError):
             RNAReadsRetriever([Path("file1"), Path("file2"), Path("file3")])
 
-    @patch("gzip.open", return_value=StringIO(MOCK_FILE_1_CONTENT))
-    def test_single_end_file_gzipped(self, _):
-        reads_retriever = RNAReadsRetriever([Path("path/to/file.gz")])
+    def test_single_end_file_gzipped(self, tmp_path):
+        with gzip.open(tmp_path / "test_file.gz", "wt", encoding="utf-8") as test_file:
+            test_file.write(
+                MOCK_FILE_1_CONTENT  # pyright: ignore[reportGeneralTypeIssues]
+            )
+
+        reads_retriever = RNAReadsRetriever([tmp_path / "test_file.gz"])
         expected_ids = [11, 41, 51]
         expected_reads = [
             (
@@ -185,19 +190,29 @@ class TestRNAReadsRetriever:
             expected_reads,
         )
 
-    @patch(
-        "builtins.open",
-        side_effect=[
-            StringIO(MOCK_FILE_1_CONTENT),
-            StringIO(MOCK_FILE_2_CONTENT),
-        ],
-    )
-    def test_paired_end_file(self, _):
+    def test_paired_end_file(self, tmp_path):
+        with gzip.open(
+            tmp_path / "test_file_1.gz", "wt", encoding="utf-8"
+        ) as test_file_1:
+            test_file_1.write(
+                MOCK_FILE_1_CONTENT  # pyright: ignore[reportGeneralTypeIssues]
+            )
+        with gzip.open(
+            tmp_path / "test_file_2.gz", "wt", encoding="utf-8"
+        ) as test_file_2:
+            test_file_2.write(
+                MOCK_FILE_2_CONTENT  # pyright: ignore[reportGeneralTypeIssues]
+            )
+
         reads_retriever = RNAReadsRetriever(
-            [Path("path/to/file1"), Path("path/to/file2")]
+            [tmp_path / "test_file_1.gz", tmp_path / "test_file_2.gz"]
         )
-        expected_ids = [21, 31, 41, 12, 32, 52]
+        expected_ids = [12, 21, 31, 32, 41, 52]
         expected_reads = [
+            (
+                "GAGCTCGATTTGCAATGAGCCGCCCCTCTTCCATAAAATCTTGCAAATTC"
+                "CGCGTCTCGGGCTCTGTCCAACACGTATGCCTCCCTTCACGATGACTTAAG"
+            ),
             (
                 "ACCGCCACGCTTACCGTTTTGGCGCTATGCTTCCATTCTGTTGTCT"
                 "ACGAGGCGATAACAACACGATACGCTCTGTTCTTACTCAGACTTATTCCGAAGCC"
@@ -207,16 +222,12 @@ class TestRNAReadsRetriever:
                 "TTTCATCTTATAGCAACACCCCGGAGTAAGCGCACTCATCCTCTCCTATC"
             ),
             (
-                "GCGTGTAATGTTATGATCTTATGCTTGTTTTAGTCCGCTAGGTTCTTTGGTGT"
-                "ACTGCCACTTTTCGATGCCATGCGCATTCTTGGGACTAGGAAGTACGA"
-            ),
-            (
-                "GAGCTCGATTTGCAATGAGCCGCCCCTCTTCCATAAAATCTTGCAAATTC"
-                "CGCGTCTCGGGCTCTGTCCAACACGTATGCCTCCCTTCACGATGACTTAAG"
-            ),
-            (
                 "AGCACCTGGTGCATTACTTCCTACCAACATGGATACAAGATGGGCCTTGCG"
                 "CTCTTTAAGTCCGCTGAGATTGCTCTATTCACTAAATCGTACGTACTGGA"
+            ),
+            (
+                "GCGTGTAATGTTATGATCTTATGCTTGTTTTAGTCCGCTAGGTTCTTTGGTGT"
+                "ACTGCCACTTTTCGATGCCATGCGCATTCTTGGGACTAGGAAGTACGA"
             ),
             (
                 "GGCCCTCGAGATACGCGCGGGAGTACGCTCCCAACTGTTTTATACCCCTGTT"
@@ -228,9 +239,10 @@ class TestRNAReadsRetriever:
             expected_reads,
         )
 
-    @patch("builtins.open", mock_open(read_data=MOCK_FILE_1_CONTENT))
-    def test_cutoff(self):
-        reads_retriever = RNAReadsRetriever([Path("path/to/file")], cutoff=10)
+    def test_cutoff(self, tmp_path):
+        with open(tmp_path / "test_file_1.fq", "wt", encoding="utf-8") as test_file:
+            test_file.write(MOCK_FILE_1_CONTENT)
+        reads_retriever = RNAReadsRetriever([tmp_path / "test_file_1.fq"], cutoff=10)
         expected_ids = [11, 41, 51]
         expected_reads = [
             "GCGTGTAATG",
