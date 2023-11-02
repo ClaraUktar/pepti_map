0bc2d46fff049dc32bd5ca6877a448d87a7249bf
Clara Uktar <clara.uktar@student.hpi.uni-potsdam.de>
2023-11-02 15:58:27 +0100

    Enable multiple threads for Bowtie index build

--- pepti_map/aligning/bowtie_wrapper.py
+++ pepti_map/aligning/bowtie_wrapper.py
@@ -7,13 +7,27 @@ import subprocess
 
 
 class BowtieWrapper:
+    def __init__(self):
+        try:
+            n_threads = os.getenv("BOWTIE_N_THREADS")
+            assert isinstance(n_threads, str)
+            n_threads = int(n_threads)
+        except (AssertionError, ValueError):
+            n_threads = multiprocessing.cpu_count()
+
+        self._n_threads = n_threads
+
     def build_index(
         self, files_to_index: List[Path], index_basename: str = "bowtie_index"
     ) -> None:
+        logging.info(f"Generating Bowtie index with {self._n_threads} threads")
+
         self._index_basename = index_basename
         # TODO: Do we want the --noref option?
         build_index_command = [
             "bowtie2-build",
+            "--threads",
+            str(self._n_threads),
             "--noref",  # do not build the index portions for paired-end alignment
             ",".join(
                 [
@@ -28,20 +42,14 @@ class BowtieWrapper:
     def produce_alignment(
         self, paths_to_sequences: List[Path], output_filepath: Path
     ) -> None:
-        try:
-            n_threads = os.getenv("BOWTIE_N_THREADS")
-            assert isinstance(n_threads, str)
-            n_threads = int(n_threads)
-        except (AssertionError, ValueError):
-            n_threads = multiprocessing.cpu_count()
-        logging.info(f"Generating Bowtie alignment with {n_threads} threads")
+        logging.info(f"Generating Bowtie alignment with {self._n_threads} threads")
 
         alignment_command = [
             "bowtie2",
             "-f",  # reads are given in FASTA format
             # TODO: Should we use "--reorder" option for same order of output as input?
             "--threads",
-            str(n_threads),
+            str(self._n_threads),
             "-x",
             self._index_basename,
             "-U",  # reads are unpaired
