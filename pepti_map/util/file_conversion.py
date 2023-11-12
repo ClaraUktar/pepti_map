f2848035b0874ef7eaa217d8a08be084c27cce33
Clara Uktar <clara.uktar@student.hpi.uni-potsdam.de>
2023-11-12 16:22:14 +0100

    Add basic bam to gff conversion

---
+++ pepti_map/util/file_conversion.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from pybedtools import BedTool
+from pybedtools.featurefuncs import bed2gff
+
+
+def convert_bam_to_gff(path_to_bam: Path):
+    bam_file = BedTool(path_to_bam)
+    bed_file = bam_file.bam_to_bed(cigar=True)
+    gff_file = bed_file.each(bed2gff)  # pyright: ignore[reportGeneralTypeIssues]
