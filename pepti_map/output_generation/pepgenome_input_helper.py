00f5aade50df5423caa743bb0646a465e53faa06
Clara Uktar <clara.uktar@student.hpi.uni-potsdam.de>
2023-11-25 23:27:26 +0100

    Generate peptide input file per subdirectory

--- pepti_map/output_generation/pepgenome_input_helper.py
+++ pepti_map/output_generation/pepgenome_input_helper.py
@@ -1,26 +1,67 @@
+from collections import defaultdict
 from pathlib import Path
 from typing import List
 
 
-def generate_peptide_input_file(
-    path_to_peptides: Path, output_path: Path, peptide_to_cluster_mapping: List[int]
-) -> None:
-    # TODO: Only use peptides for set
-    peptides = []
-    with open(path_to_peptides, "rt", encoding="utf-8") as peptides_file:
-        for line in peptides_file:
-            peptides.append(
-                line.split("\t")[0].strip()
-            )  # ignores group information if present
+class PepGenomeInputHelper:
+    def __init__(
+        self, path_to_peptides: Path, path_to_peptide_to_cluster_mapping: Path
+    ):
+        self._peptides = []
+        with open(path_to_peptides, "rt", encoding="utf-8") as peptides_file:
+            for line in peptides_file:
+                self._peptides.append(
+                    line.split("\t")[0].strip()
+                )  # ignores group information if present
 
-    with open(output_path, "wt", encoding="utf-8") as new_input_file:
-        for peptide_index, peptide in enumerate(peptides):
-            if (
-                peptide_to_cluster_mapping[peptide_index] == -1
-            ):  # Exclude peptides that were not part of the pipeline
-                continue
-            # Use 1 as default for Sample, PSMs and Quant
-            new_input_file.write("\t".join(["1", peptide, "1", "1"]) + "\n")
+        # Reverse mapping to get cluster_id -> peptide_id
+        self._cluster_to_peptide_mapping: "defaultdict[int, List[int]]" = defaultdict(
+            list
+        )
+        with open(
+            path_to_peptide_to_cluster_mapping, "rt", encoding="utf-8"
+        ) as peptide_to_cluster_mapping:
+            for peptide_id, line in enumerate(peptide_to_cluster_mapping):
+                cluster_id = int(line.strip())
+                # Exclude peptides that were too short to be included
+                if cluster_id == -1:
+                    continue
+                self._cluster_to_peptide_mapping[cluster_id].append(peptide_id)
+
+    def generate_peptide_input_file(
+        self, output_path: Path, merged_indexes: List[int]
+    ) -> None:
+        peptide_already_written: List[bool] = [
+            False for _ in range(len(self._peptides))
+        ]
+        with open(output_path, "wt", encoding="utf-8") as new_input_file:
+            for cluster_id in merged_indexes:
+                peptide_ids = self._cluster_to_peptide_mapping[cluster_id]
+                for peptide_id in peptide_ids:
+                    if peptide_already_written[peptide_id]:
+                        continue
+                    # Use 1 as default for Sample, PSMs and Quant
+                    new_input_file.write(
+                        "\t".join(["1", self._peptides[peptide_id], "1", "1"]) + "\n"
+                    )
+                    peptide_already_written[peptide_id] = True
+
+    def generate_all_peptide_input_files(
+        self, output_paths: List[Path], path_to_merged_indexes: Path
+    ) -> None:
+        # TODO: Refactor to use code from match merger
+        merged_indexes: List[List[int]] = []
+        with open(
+            path_to_merged_indexes, "rt", encoding="utf-8"
+        ) as peptide_indexes_file:
+            for line in peptide_indexes_file:
+                line = line.strip()
+                merged_indexes.append(
+                    [int(match_elem) for match_elem in line.split(",")]
+                )
+
+        for set_index, output_path in enumerate(output_paths):
+            self.generate_peptide_input_file(output_path, merged_indexes[set_index])
 
 
 def generate_gff_input_file(path_to_gff: Path, output_path: Path) -> None:
